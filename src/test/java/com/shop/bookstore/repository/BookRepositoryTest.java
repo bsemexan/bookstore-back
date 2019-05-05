09d1cfd19baaf935cbd9697aff2ef469bfb7fe84
bsemexan <bsemexan@gmail.com>
2019-05-05 18:25:26 -0400

    book repository test init - counting books

--- src/test/java/com/shop/bookstore/repository/BookRepositoryTest.java
+++ src/test/java/com/shop/bookstore/repository/BookRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.shop.bookstore.repository;
 
+import com.shop.bookstore.model.Book;
+import com.shop.bookstore.model.Language;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -7,6 +9,10 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -16,10 +22,17 @@ public class BookRepositoryTest {
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClass(BookRepository.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addClass(Book.class)
+                .addClass(Language.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/test-persistence.xml", "persistence.xml");
     }
-
+    @Inject
+    private BookRepository bookRepository;
     @Test
     public void create() throws Exception{
+        //counting book
+        assertEquals(0,bookRepository.findAll().size());
+        assertEquals(Long.valueOf(0), bookRepository.countAll());
     }
 }
